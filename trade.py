0225b8f3b1a8bfc8522088a63bef510b509191b2
way <cuifuqian@hotmail.com>
2016-01-27 15:17:44 +0800

    finish trade.stop() function

--- trade.py
+++ trade.py
@@ -15,21 +15,17 @@ class trade(object):
         size = len(self.feed)
         try:
             balance = self.user.balance
-            balance_log = self.LOGPATH + 'balance.log'
             err_log = self.LOGPATH + 'err.log'
             trade_log = self.LOGPATH + 'trade.log'
             start_time = time.strftime("%Y-%m-%d %H:%M",time.localtime())
-            account_amount = balance[0]['asset_balance']
             reserve_amount = balance[0]['enable_balance']
-            market_value = enable_balance[0]['market_value']
-            BALANCE_LOG = open(balance_log, 'a')
             ERR_LOG = open(err_log, 'a')
             TRADE_LOG = open(trade_log, 'a')
 
             if reserve_amount < size * 10000:
                 MASSAGE = '"%s": There is no enough to start the trade\n' %(start_time)
-                BALANCE_LOG.write(MASSAGE)
-                BALANCE_LOG.close()
+                ERR_LOG.write(MASSAGE)
+                ERR_LOG.close()
 
             else:
                 share = reserve_amount / size
@@ -38,44 +34,43 @@ class trade(object):
                     price = float(quotes.loc[[0], 'price']) * 100 # the price for one hand, 100 share
                     last_price = price / 100
                     quantity = int(share // price * 100)
-                    user.buy(stock, price=last_price, amount=quantity)
-                    MASSAGE = '%s,%s,%f,%d\n' %(start_time, stock, last_price, quantity)
+                    self.user.buy(stock, price=last_price, amount=quantity)
+                    MASSAGE = '%s,%s,%f,%d,buy\n' %(start_time, stock, last_price, quantity)
                     TRADE_LOG.write(MASSAGE)
                 TRADE_LOG.close()
-        except KeyError:
+        except:
             pass
 
-#stop function requres completed
     def stop(self):
         try:
+            position = self.user.position
             err_log = self.LOGPATH + 'err.log'
+            trade_log = self.LOGPATH + 'trade.log'
+            ERR_LOG = open(err_log, 'a')
+            TRADE_LOG = open(trade_log, 'a')
+            balance = self.user.balance
             trade_log = self.LOGPATH + 'trade.log'
             stop_time = time.strftime("%Y-%m-%d %H:%M",time.localtime())
             account_amount = balance[0]['asset_balance']
-            reserve_amount = balance[0]['enable_balance']
-            market_value = enable_balance[0]['market_value']
-            BALANCE_LOG = open(balance_log, 'a')
-            ERR_LOG = open(err_log, 'a')
+            market_value = balance[0]['market_value']
             TRADE_LOG = open(trade_log, 'a')
 
-            if market_value < 100:
+            if market_value / account_amount * 100 < 3:
                 MASSAGE = '"%s": stocks have been clearred\n' %(stop_time)
-                BALANCE_LOG.write(MASSAGE)
-                BALANCE_LOG.close()
+                ERR_LOG.write(MASSAGE)
+                ERR_LOG.close()
 
             else:
-                share = reserve_amount / size
-                for stock in self.feed.load():
-                    quotes = ts.get_realtime_quotes(stock)
-                    price = float(quotes.loc[[0], 'price']) * 100 # the price for one hand, 100 share
-                    last_price = price / 100
-                    quantity = int(share // price * 100)
-                    user.buy(stock, price=last_price, amount=quantity)
-                    MASSAGE = '%s,%s,%f,%d\n' %(stop_time, stock, last_price, quantity)
-                    TRADE_LOG.write(MASSAGE)
+                for term in position:
+                    if term['stock_code'] in self.feed.load():
+
+                        quotes = ts.get_realtime_quotes(term['stock_code'])
+                        price = float(quotes.loc[[0], 'price']) * 100 # the price for one hand, 100 share
+                        last_price = price / 100
+                        quantity = int(term['enable_amount'])
+                        self.user.sell(term['stock_code'], price=last_price, amount=quantity)
+                        MASSAGE = '%s,%s,%f,%d,sell\n' %(stop_time, term['stock_code'], last_price, quantity)
+                        TRADE_LOG.write(MASSAGE)
                 TRADE_LOG.close()
-        except KeyError:
+        except:
             pass
-
-
-
