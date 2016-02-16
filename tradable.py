78319f41ee72c514b3a98f18e94339bfc2c6237b
way <cuifuqian@hotmail.com>
2016-02-16 14:21:22 +0800

    using reatime data to check stock

--- tradable.py
+++ tradable.py
@@ -2,25 +2,22 @@
 # encoding: utf-8
 
 #verify the tradable stock
-#load the yesterdayline before use this function
-import tushare
 
-def tradable(yesterdayline):
+import tushare
+import pandas as pd
+
+def tradable():
 
     base_info = tushare.get_stock_basics()
-    yesterdayline['limit'] = yesterdayline.high - yesterdayline.low
-    yesterdayline = yesterdayline[yesterdayline.limit != 0]
-    base_info[u'price'] = 0
-    base_info.price = yesterdayline.close
+    trade_info = tushare.get_today_all()
+#remove the duplicates stocks and reconstruct a new df with stock code as index
+    trade_info = trade_info.drop_duplicates('code')
+    trade_info = pd.DataFrame(trade_info.values, columns=trade_info.columns, index=trade_info.code)
+    trade_info_tradeable = trade_info[trade_info.changepercent < 9.93]
+   
+    base_info[u'price'] = trade_info_tradeable.trade
+
     base_info = base_info.dropna()
-    
-# add tradable column for base_info dataframe
-#    base_info[u'tradable'] = 0
-#    base_info[u'price'] = 0
-#    for i in yesterdayline.index :
-#        base_info.loc[i,u'tradable'] = 1
-#        base_info.loc[i, u'price'] = yesterdayline.loc[i, u'close']
+
             
     return base_info
-
-
