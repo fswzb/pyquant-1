a2ed93d4ee78121c137c477d5d233026df6a0bd5
way <cuifuqian@hotmail.com>
2016-03-15 13:27:22 +0800

    reslove package related

--- weipan.py
+++ weipan.py
@@ -7,21 +7,23 @@
 
 from sqlalchemy import create_engine
 from download20day import download20day
-import signal.flag
-import signal.zxb_limit
+import sys
+sys.path.append('signal')
+import flag
+import zxb_limit
 import time
 
 
 
-account_flag = signal.flag.flag()
-limit = signal.zxb_limit.zxb_limit(-5.2)
+account_flag = flag.flag()
+limit = zxb_limit.zxb_limit(-5.2)
 if account_flag.read_flag() in (4, 8):
     if limit.zxb_signal():
         account_flag.set_flag(account_flag.read_flag() + 1)
     else:
         data1 = download20day()
         data2 = download20day(90)
-engine = create_engine('mysql://root:6598518@127.0.0.1/stock?charset=utf8')
+engine = create_engine('mysql://root:123456@127.0.0.1/stock?charset=utf8')
 table = 'yue' + time.strftime('%Y-%m-%d', time.localtime())
 table1 = 'yue2' + time.strftime('%Y-%m-%d', time.localtime())
 
